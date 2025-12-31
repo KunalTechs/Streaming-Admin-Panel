e63f93464756a6b1214e5f62caf3a23fc23f72ef
Kunal Sawle <ksawle1515@gmail.com>
2026-01-01 04:08:18 +0530

    feat(routes): add routes for deleteAdmin, UpdateAdmin,updatepassword and updateprofile

--- backend/admin-auth-service/src/routes/authRoutes.js
+++ backend/admin-auth-service/src/routes/authRoutes.js
@@ -6,9 +6,14 @@ import {
     handleRefreshToken,
     getProfile,
     getAllAdmins,
+    deleteAdmin,
+    updateAdminRole,
+    updatePassword,
+    updateProfile,
+
 } from "../controllers/authController.js";
-import { protect } from "../middlewares/auth.middleware.js";
-import { restrictTo } from "../middlewares/role.middleware.js";
+import { protect } from "../middlewares/authMiddleware.js";
+import { restrictTo } from "../middlewares/roleMiddleware.js";
 
 const router = express.Router();
 
@@ -17,6 +22,19 @@ route:-POST /api/auth/register
 work:- Create a new admin account (Public or Superadmin only depending on your choice)
 */
 router.post("/register",protect,restrictTo("superadmin"),register);
+
+// Only a Superadmin can see the full list of other administrators
+router.get("/all-admins", 
+    protect, 
+    restrictTo("superadmin"), 
+    getAllAdmins
+);
+
+//delete Admin by superadmin
+router.delete("/delete-admin/:id",protect,restrictTo("superadmin"),deleteAdmin)
+
+// UpdateAdmin role by superadmin
+router.patch("/update-role/:id", protect, restrictTo("superadmin"), updateAdminRole);
 
 /*
 route:- POST /api/auth/login
@@ -41,12 +59,12 @@ route:-GET /api/auth/profile
 work:- Get current admin data (Example of a Protected Route)
 */
 router.get("/profile", protect, getProfile);
+
+// Self-service routes (Any logged-in admin)
+router.patch("/update-my-password",protect, updatePassword);
+
+router.patch("/update-me", protect, updateProfile);
+
 export default router;
 
-// Only a Superadmin can see the full list of other administrators
-router.get("/all-admins", 
-    protect, 
-    restrictTo("superadmin"), 
-    getAllAdmins
-);
 
