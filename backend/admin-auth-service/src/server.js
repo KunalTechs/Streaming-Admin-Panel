5bd4e08035fbb3c5a60a2dd0e976075baf144125
Kunal Sawle <ksawle1515@gmail.com>
2026-01-03 03:10:47 +0530

    feat: complete kafka integration with admin auth service

--- backend/admin-auth-service/src/server.js
+++ backend/admin-auth-service/src/server.js
@@ -1,14 +1,34 @@
 import "dotenv/config";
 import connectDB from "./config/db.js";
 import app from "./app.js";
+import { initTopics } from "./config/kafka.js";
+import { connectProducer } from "./events/producer.js";
 
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT;
 
 //Database Connection
-await connectDB()
+const startServer = async () => {
+  try {
+    // Connect To MongoDB
+    await connectDB();
 
-app.get('/', (req,res) => res.send("Server is live..."))
+    // Initialize Kafka Topics (The Highways)
+    await initTopics();
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on adress http://localhost:${PORT}`)
-} )
+    // Connect the Kafka Producer
+    await connectProducer();
+
+    // Start The Express Server
+
+    app.listen(PORT, () => {
+      console.log(`Server running on adress http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1); // Stop the app if any infrastructure fails
+  }
+};
+
+app.get("/", (req, res) => res.send("Server is live..."));
+
+startServer();
