1210c5cda1d1f26791c7fe3a5b037e960c4c61a3
Kunal Sawle <ksawle1515@gmail.com>
2026-01-09 04:46:06 +0530

    feat(server): integrate database connection and kafka consumer:-
    Initialized the Prisma client with the MariaDB adapter on server startup.
    
    Established the Kafka admin.consumer connection to start listening for streaming events
    
    Wrapped startup logic in an asynchronous function to ensure the database is ready before the consumer begins processing.

--- backend/video-service/src/server.js
+++ backend/video-service/src/server.js
@@ -1,14 +1,31 @@
 import "dotenv/config";
-import app from "./app.js"
-
+import app from "./app.js";
+import prisma from "./config/prisma.js";
+import { initAdminConsumer } from "./events/admin.consumer.js";
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () =>{
-console.log(`Server running on address http://localhost:${PORT}`)
-});
+const startServer = async () => {
+  try {
+    //Test Database Connection
+    await prisma.$connect();
+    console.log("prisma is running");
+    //Start Kafka Consumer
+    // This starts watching for "ADMIN_CREATED" events from the Auth Service
+    await initAdminConsumer();
+    console.log("kafka consumer is running");
 
+    app.listen(PORT, () => {
+      console.log(`Server running on address http://localhost:${PORT}`);
+    });
+
+  } catch (error) {
+    console.error(" Failed to start server:", error);
+    process.exit(1);
+  }
+};
 app.get("/", (req, res) => res.send("Server is live..."));
 
+startServer();
 
 
