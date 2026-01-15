eb41ca2a4e980ac7a479e6fe2208ea3f48ba1dcc
Kunal Sawle <ksawle1515@gmail.com>
2026-01-16 02:40:40 +0530

    fix(prisma.js): remove mariadb adapter method to for prisma connect and connect to mysql db in docker

--- backend/video-service/src/config/prisma.js
+++ backend/video-service/src/config/prisma.js
@@ -1,20 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaMariaDb } from '@prisma/adapter-mariadb';
-import mariadb from 'mariadb';
 
-// 1. Setup the connection pool (Better for performance)
-const pool = mariadb.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  connectionLimit: 5
-});
 
-// 2. Initialize the adapter
-const adapter = new PrismaMariaDb(pool);
-
-// 3. Create the client with the adapter
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 export default prisma;
