7f45796913f3b11dea0e78e893491700141729a8
Kunal Sawle <ksawle1515@gmail.com>
2026-01-20 02:20:21 +0530

    feat(app):implement jwt cookie

--- backend/video-service/src/app.js
+++ backend/video-service/src/app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import videoRoutes from "./routes/videoRoutes.js";
+import cookieParser from 'cookie-parser';
 
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(cookieParser());
 
 app.use("/api/videos", videoRoutes);
 
