7e13b87930080950c9fb16c715fd83e1dc0066a1
Kunal Sawle <ksawle1515@gmail.com>
2026-01-22 03:39:38 +0530

    feat(kafka.js):add ADMIN_CREATED and ADMIN_DELETED topics

--- backend/admin-auth-service/src/config/kafka.js
+++ backend/admin-auth-service/src/config/kafka.js
@@ -1,31 +1,32 @@
-import {Kafka} from "kafkajs"
+import { Kafka } from "kafkajs";
 import "dotenv/config";
 
 const kafka = new Kafka({
-    clientId: "admin-auth-services",
-    brokers: [process.env.KAFKA_BROKER]
+  clientId: "admin-auth-services",
+  brokers: [process.env.KAFKA_BROKER],
 });
 
-export const initTopics = async () =>{
-    const admin = kafka.admin();
-    try {
-        await admin.connect();
-        await admin.createTopics({
-            waitForLeaders:true,
-            topics: [
-                {topic:"admin-events", numPartitions: 3},
-                {topic: "audit-logs", numPartitions:3}
-            ],
-        });
-        console.log("Kafka Topics InItialized")
-    } catch (error) {
-        if(!error.message.includes("already exists")){
-            console.error( "Kafka Admin Error", error)
-        }
-        
-    } finally {
-        await admin.disconnect();
+export const initTopics = async () => {
+  const admin = kafka.admin();
+  try {
+    await admin.connect();
+    await admin.createTopics({
+      waitForLeaders: true,
+      topics: [
+        { topic: "admin-events", numPartitions: 3 },
+        { topic: "audit-logs", numPartitions: 3 },
+        { topic: "ADMIN_CREATED", numPartitions: 1 },
+        { topic: "ADMIN_DELETED", numPartitions: 1 },
+      ],
+    });
+    console.log("Kafka Topics InItialized");
+  } catch (error) {
+    if (!error.message.includes("already exists")) {
+      console.error("Kafka Admin Error", error);
     }
+  } finally {
+    await admin.disconnect();
+  }
 };
 
 export default kafka;
