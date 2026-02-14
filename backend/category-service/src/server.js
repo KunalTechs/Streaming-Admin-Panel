d577ec4898e5d7749d1c7670da40f89dc90d895a
Kunal Sawle <ksawle1515@gmail.com>
2026-02-15 02:28:31 +0530

    feat(server.js):implement prima in category service

--- backend/category-service/src/server.js
+++ backend/category-service/src/server.js
@@ -1,7 +1,16 @@
 import "dotenv/config";
 import app from "./app.js";
+import { connectProducer } from "./events/category.producer.js";
+import prisma from "./config/prisma.js";
+
 
 const PORT = process.env.PORT;
+
+await prisma.$connect();
+ console.log("prisma is running");
+
+//Start the Mouth of Kafka
+await connectProducer();
 
 app.listen(PORT, () =>{
   console.log(`Server running on address http://localhost:${PORT}`);
