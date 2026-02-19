ed0c3408519fa7f572c50a2f7728bb07b47c5d2d
Kunal Sawle <ksawle1515@gmail.com>
2026-02-20 00:58:15 +0530

    feat(category controller):add update category and implement redis as caching

--- backend/category-service/src/controllers/categoryController.js
+++ backend/category-service/src/controllers/categoryController.js
@@ -1,14 +1,33 @@
 import prisma from "../config/prisma";
+import redisClient from "../config/redis.js";
 import slugify from "slugify";
 import { emitCategoryEvent } from "../events/category.producer";
+
+const CACHE_KEY = "categories:all";
 
 // 1. Get All
 export const getallCategories = async(resizeBy, req) =>{
     try {
+        // Check Redis Cache
+        const cachedCategories = await redisClient.get(CACHE_KEY);
+
+        if(cachedCategories){
+            console.log(" Serving from Redis Cache");
+            return res.json({
+                success: true,
+                categories: JSON.parse(cachedCategories),
+                source: "cache"
+            });   
+        }
+
+        // If not in cache, fetch from MySQL
         const categories = await prisma.category.findMany({
             orderBy:{name:'asc'}
-        })
-        res.json({success: true, categories})
+        });
+
+        // Save to Redis (Expire in 1 hour / 3600 seconds)
+        await redisClient.setEx(CACHE_KEY, 3600, JSON.stringify(categories));
+        res.json({success: true, categories, source: "database"})
         
     } catch (error) {
         res.status(500).json({ success: false, message: error.message});
@@ -26,6 +45,9 @@ export const createCategory = async (req, res) =>{
             data: {name, slug, description}
         })
 
+        // Delete cache because new data exists
+        await redisClient.del(CACHE_KEY);
+
         await emitCategoryEvent("CATEGORY_CREATED", category);
         res.status(201).json({success: true, category});
         
@@ -40,10 +62,41 @@ export const deleteCategory = async (req, res) =>{
 
         await prisma.category.delete({where: {id}});
 
+        // Delete cache because data is removed
+        await redisClient.del(CACHE_KEY);
+
         // Tell Video Service to clean up its links!
         await emitCategoryEvent("CATEGORY_DELETED", {id});
         res.json({success: true, message: "Category deleted successfully"});
     } catch (error) {
         res.status(400).json({success: false, message: error.message})
+    }
+}
+
+export const updateCategory = async (req,res) =>{
+    try {
+        const {id} =  req.params;
+        const {name, description} = req.body;
+
+        const updateData =  {name, description};
+        if (name) {
+            updateData.slug = slugify(name, {lower: true, strict: true});
+        }
+
+        const updatedCategory = await prisma.category.update({
+            where: {id},
+            data: updateData
+        });
+
+        //Invalidate Redis Cache
+        await redisClient.del(CACHE_KEY);
+
+        //Notify other services via Kafka
+        await emitCategoryEvent("CATEGORY_UPDATED", updatedCategory);
+
+        res.json({success: true, catgeory: updateCategory});
+        
+    } catch (error) {
+       res.status(400).json({success: false, message: error.message})
     }
 }
