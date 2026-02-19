84cbdbafbdcddd02f894e397660002975cc15527
Kunal Sawle <ksawle1515@gmail.com>
2026-02-20 01:01:34 +0530

    feat(app.js):implement cookie parser in auth service and in category service

--- backend/admin-auth-service/src/app.js
+++ backend/admin-auth-service/src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
