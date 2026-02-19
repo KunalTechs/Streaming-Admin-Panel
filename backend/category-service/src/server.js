796b2d57481bd76a503ddc2eec102899363d7a96
Kunal Sawle <ksawle1515@gmail.com>
2026-02-20 01:02:52 +0530

    feat(server.js):connect redis for cache

--- backend/category-service/src/server.js
+++ backend/category-service/src/server.js
@@ -2,20 +2,29 @@ import "dotenv/config";
 import app from "./app.js";
 import { connectProducer } from "./events/category.producer.js";
 import prisma from "./config/prisma.js";
+import { connectRedis } from "./config/redis.js";
 
 
 const PORT = process.env.PORT;
 
+const startServer = async () => {
+
+//connect to prisma
 await prisma.$connect();
  console.log("prisma is running");
 
 //Start the Mouth of Kafka
 await connectProducer();
 
+// Connect to Redis
+await connectRedis();
+
+
 app.listen(PORT, () =>{
   console.log(`Server running on address http://localhost:${PORT}`);
 })
 
+};
 app.get("/", (req, res) => res.send("Server is live..."));
 
-
+startServer();
