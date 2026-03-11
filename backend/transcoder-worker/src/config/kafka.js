a71f75b35876b4cbce22bcdae4d260edabb693b1
Kunal Sawle <ksawle1515@gmail.com>
2026-03-12 02:02:07 +0530

    feat(transcoder-kafka):consumer producer connection settings

---
+++ backend/transcoder-worker/src/config/kafka.js
@@ -0,0 +1,10 @@
+import Kafka from 'kafkajs';
+import 'dotenv/config';
+
+const kafka = new Kafka ({
+    clientId: "transcoder-worker",
+    brokers: [process.env.KAFKA_BROKER],
+});
+
+export const consumer = kafka.consumer({groupId: "transcoder-worker-group"});
+export const producer = kafka.producer({groupId: "transcoder-worker-group"})
