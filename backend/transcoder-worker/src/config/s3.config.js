1e6e044f710c2fa57f7fbf307107dfb0d8535ac0
Kunal Sawle <ksawle1515@gmail.com>
2026-03-12 01:45:09 +0530

    feat(transcoder-s3config):setup or implement S3 from aws for transcoding the video

---
+++ backend/transcoder-worker/src/config/s3.config.js
@@ -0,0 +1,12 @@
+import {S3Client} from '@aws-sdk/client-s3';
+import 'dotenv/config';
+
+export const s3Client = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+    }
+});
+
+export const BUCKET_NAME = process.env.BUCKET_NAME;
