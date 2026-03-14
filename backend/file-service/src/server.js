d862eb880b6f794c1b5951b795f33cbacfb0c23c
Kunal Sawle <ksawle1515@gmail.com>
2026-03-15 00:57:20 +0530

    feat(file-service):start kafka consumer by implementing initkafkaconsumer in server

--- backend/file-service/src/server.js
+++ backend/file-service/src/server.js
@@ -1,8 +1,13 @@
 import "dotenv/config";
 import app from "./app.js";
+import { initKafkaConsumer } from "./events/fileservice.consumer.js";
 
 
 const PORT = process.env.PORT;
+
+await initKafkaConsumer();
+ console.log("kafka consumer is running");
+ 
 app.listen(PORT, () => {
       console.log(`Server running on address http://localhost:${PORT}`);
     });
