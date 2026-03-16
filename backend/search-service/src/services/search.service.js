d1c41e0e2f95dc28e98a809cfbf298f6d7ccc459
Kunal Sawle <ksawle1515@gmail.com>
2026-03-17 04:23:37 +0530

    feat(search-service):add filter for category search and add highlight method to get which field matched

--- backend/search-service/src/services/search.service.js
+++ backend/search-service/src/services/search.service.js
@@ -1,21 +1,40 @@
 import { esClient } from "../config/elasticsearch.js";
+import { VIDEO_INDEX } from "../constants/indexNames.js";
 
 export const executeSearch = async (searchTerm) => {
-    const result = await esClient.search({
-        index: 'videos',
-        query: {
-            multi_match :{
-                query: searchTerm,
-                fields: ['title^3', 'description', 'tags'], //// Title is 3x more important
-                fuzziness: 'AUTO', // Handles typos!
-            }
-        }
-    });
+  const result = await esClient.search({
+    index: VIDEO_INDEX,
 
-    // Elasticsearch returns a deep object, we simplify it for the frontend
-    return result.hits.hits.map(hit => ({
-        id: hit._id,
-        ...hit._source,
-        score: hit._score //Search relevance score
-    }));
+      query: {
+        bool: {
+          must: [
+            {
+              multi_match: {
+                query: searchTerm,
+                fields: ["title^3", "description", "tags"], //// Title is 3x more important
+                fuzziness: "AUTO", // Handles typos!
+                operator: "and",
+              },
+            },
+          ],
+          // Example: Filter by category if the user selects one
+         filter: filters.category ? [{ term: { "category.keyword": filters.category } }] : [],
+        },
+      },
+      // Highlight the words that matched
+      highlight: {
+        fields: {
+          title: {},
+          description: {},
+        },
+      },
+  });
+
+  // Elasticsearch returns a deep object, we simplify it for the frontend
+  return result.hits.hits.map((hit) => ({
+    id: hit._id,
+    ...hit._source,
+    score: hit._score, //Search relevance score 
+    highlights: hit.highlight,
+  }));
 };
